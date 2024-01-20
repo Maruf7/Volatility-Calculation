1f1f527ecdfb9df613b203579a6292aeaa8b0544
unknown <marufshaikh211@gmail.com>
2024-01-20 15:56:10 +0530

    Refactoring

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template, request
 import pandas as pd
 import math 
 import json
-from waitress import serve
 
 app = Flask(__name__)
 
 def calculateVolatility(file):
+    # Calculate all Volatility values
     niftyDf = pd.read_excel(file)
     niftyDf.columns = niftyDf.columns.str.strip()
     niftyDf["Returns"] = niftyDf["Close"].pct_change(1)
@@ -22,9 +22,10 @@ def calculateVolatility(file):
     return valDic
 
 
-@app.route('/details', methods = ['POST'])
-def details():
+def checkFile(request):
+    #Check if the request has file or file path
 
+    calVal = None
     if request.method == 'POST' and request.form.get('path', '') == '' and request.files['file'].filename != '':
         file = request.files['file']
         calVal = calculateVolatility(file)
@@ -33,33 +34,33 @@ def details():
     elif request.method == 'POST' and request.form.get('path', '') != '':
         file = request.form['path']
         calVal = calculateVolatility(file)
-        return render_template("show.html", valueBundle=calVal)
-    
+        
+    return calVal
+
+
+
+
+@app.route('/showDetails', methods = ['POST'])
+def showDetails():
+    volatilityVal = checkFile(request)
+    if volatilityVal != None:
+        return render_template("show.html", valueBundle=volatilityVal)
     else:
         return render_template("error.html")
-    
+
 
 @app.route('/volatility', methods = ['POST'])
 def volatility():
-    if request.method == 'POST' and request.form.get('path', '') == '' and request.files['file'].filename != '':
-        file = request.files['file']
-        calVal = calculateVolatility(file)
-        return calVal
-    
-    elif request.method == 'POST' and request.form.get('path', '') != '':
-        file = request.form['path']
-        calVal = calculateVolatility(file)
-        return calVal
-    
+    volatilityVal = checkFile(request)
+    if volatilityVal != None:
+        return volatilityVal
     else:
         return render_template("error.html")
     
     
-
 @app.route('/')
 def home():
     return render_template("upload.html")
-
 
 if __name__ == '__main__':   
     app.run(debug=True, port=5000)
